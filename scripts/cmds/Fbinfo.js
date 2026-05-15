aec9872523dfd2eabf5d25331c8445af33dc598b
oneshotemran-coder <oneshotemran@gmail.com>
2026-05-16 00:26:33 +0600

    Update Fbinfo.js

--- scripts/cmds/Fbinfo.js
+++ scripts/cmds/Fbinfo.js
@@ -47,7 +47,7 @@ module.exports = {
 🚻 Gender: ${gender}  
 🔗 Profile: https://facebook.com/${uid}  
   
-─꯭─⃝͎̽𝗙𝗔𝗛𝗜𝗠 𝐀𝐑 𝐁𝐎𝐓‣᭄𓆪___//😽🩵🪽`  
+─꯭─⃝͎̽IMRAN AR BOT‣᭄𓆪___//😽🩵🪽`  
       );  
   
     } catch (err) {  
